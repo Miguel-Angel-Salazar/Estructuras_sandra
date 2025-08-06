bff1a12149dbc48fac94a5a5c44452014270be24
Miguel-Angel-Salazar <miguelangelguarnizosalazar@gmail.com>
2025-08-06 01:24:55 +0000

    new

--- recursion/taller/3.py
+++ recursion/taller/3.py
@@ -2,5 +2,5 @@ def superdigito(num):
   if num < 10:
     return num
   if num >= 10 :
-    return superdigito(num // 10 + num % 10)
+    return superdigito((num // 10)) + (num % 10)
 print(superdigito(9875))
--- recursion/taller/6.py
+++ recursion/taller/6.py
@@ -1,12 +1,11 @@
-def formas_escalera(n):
-    if n < 0:
+def formas_escalera(n, actual=0):
+    if actual == n:
+        return 1
+    if actual > n:
         return 0
-    if n == 0:
-      return 1
-    else:
-        return formas_escalera(n - 1) + formas_escalera(n - 2)
+    return formas_escalera(n, actual + 1) + formas_escalera(n, actual + 2)
 
 print(formas_escalera(1))
 print(formas_escalera(3))
 print(formas_escalera(4))
-print(formas_escalera(5))
+print(formas_escalera(6))
